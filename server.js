f8063b9120e4816e8eea474a0b005d90d56e2530
Ray <raysmithdev@gmail.com>
2017-01-23 16:29:55 -0800

    Part 5

--- server.js
+++ server.js
@@ -3,33 +3,66 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const Todo = require('./models/todo')
+const router = express.Router()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.get('/todo', (req, res) => {
-  Todo.find((err, todos) => {
-    if(err) {
-      res.send(err)
-    } else {
-      res.json(todos)
-    }
-  })
-})
-
-app.post('/todo', (req, res) => {
-  const todo = new Todo()
-  todo.text = 'Test2'
-
-  todo.save((err, todo) => {
-    if(err) {
-      res.send(err)
-    } else {
-      res.json(todo)
-    }
-  })
-})
 
 mongoose.connect('mongodb://localhost:27017/todo')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+router.use((req, res, next) => {
+    console.log('Something is happening.')
+    next()
+});
+
+router.route('/todo')
+
+  .get((req, res) => {
+    Todo.find((err, todos) => {
+      if(err) {
+        res.send(err)
+      } else {
+        res.json(todos)
+      }
+    })
+  })
+
+  .post((req, res) => {
+    const todo = new Todo()
+    todo.text = req.body.postName
+
+    todo.save((err, todo) => {
+      if(err) {
+        res.send(err)
+      } else {
+        Todo.find((err, todos) => {
+          if(err) {
+            res.send(err)
+          } else {
+            res.json(todos)
+          }
+        })
+      }
+    })
+  })
+
+  .delete((req, res) => {
+    Todo.findByIdAndRemove(req.body.id, (err, todo) => {
+      if(err) {
+        res.send(err)
+      } else {
+        Todo.find((err, todos) => {
+          if(err) {
+            res.send(err)
+          } else {
+            res.json(todos)
+          }
+        })
+      }
+    })
+  })
+
+app.use('/', router);
 
 app.listen(8080, () => console.log('Server is up & running!'))
